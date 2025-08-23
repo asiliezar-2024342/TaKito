2e2bc68d262906462aea82ccd5f666c34c9e3c2a
jparedes-2024240 <jparedes-2024240@kinal.edu.gt>
2025-08-23 17:00:07 -0600

    feat: Agregar funcionalidad de CRUD al Usuario, y arreglo de inicio de sesión.

--- src/java/controlador/Controlador.java
+++ src/java/controlador/Controlador.java
@@ -1,22 +1,34 @@
 package controlador;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Date;
 import java.util.List;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.servlet.http.Part;
+import modelo.Cliente;
+import modelo.ClienteDAO;
 import modelo.Resena;
 import modelo.ResenaDAO;
 import modelo.Usuario;
 import modelo.UsuarioDAO;
 
+@MultipartConfig
 public class Controlador extends HttpServlet {
 
     Usuario usuario = new Usuario();
     UsuarioDAO usuarioDao = new UsuarioDAO();
     Resena resena = new Resena();
     ResenaDAO resenaDao = new ResenaDAO();
+    Cliente cliente = new Cliente();
+    ClienteDAO clienteDao = new ClienteDAO();
+    int codUsuario;
     int codResena;
 
     /**
@@ -32,13 +44,31 @@ public class Controlador extends HttpServlet {
             throws ServletException, IOException {
         String menu = request.getParameter("menu");
         String accion = request.getParameter("accion");
-
+        Usuario usuarioActual = new Usuario();
+        String nombres;
+        String apellidos;
+        
         if (menu.equals("Principal")) {
-
+            usuarioActual = (Usuario)request.getAttribute("usuario");
+            HttpSession usuarioSesion = request.getSession();
+            
+            if(usuarioActual.getCargo().toString().equalsIgnoreCase("Consumidor")){
+                nombres = clienteDao.buscarUsuario(usuarioActual.getCodigoUsuario()).getPrimerNombreCliente();
+                apellidos = clienteDao.buscarUsuario(usuarioActual.getCodigoUsuario()).getPrimerApellidoCliente();
+                
+                usuarioSesion.setAttribute("usuarioActual", usuarioActual);
+                usuarioSesion.setAttribute("nombreCliente", nombres);
+                usuarioSesion.setAttribute("apellidoCliente", apellidos);
+            } else {
+                usuarioSesion.setAttribute("usuarioActual", usuarioActual);
+            }
+            
             // ANIMACIÓN DE TRANSICIÓN NO TOCAR
             request.setAttribute("jspFinal", "Principal.jsp");
             request.getRequestDispatcher("Inicial.jsp").forward(request, response);
-
+            
+            response.sendRedirect("Principal.jsp");
+ 
         } else if (menu.equals("PrincipalContenido")) {
             // ANIMACIÓN DE TRANSICIÓN NO TOCAR
             request.setAttribute("jspFinal", "PrincipalContenido.jsp");
@@ -48,7 +78,7 @@ public class Controlador extends HttpServlet {
 
             switch (accion) {
                 case "Listar":
-                    List listaUsuarios = usuarioDao.listar();
+                    List<Usuario> listaUsuarios = usuarioDao.listar();
 
                     request.setAttribute("usuarios", listaUsuarios);
                     break;
@@ -62,12 +92,80 @@ public class Controlador extends HttpServlet {
                         usuario.setContrasenaUsuario(contrasena);
 
                         usuarioDao.agregar(usuario);
+                        
+                        cliente.setPrimerNombreCliente("---");
+                        cliente.setSegundoNombreCliente("---");
+                        cliente.setPrimerApellidoCliente("---");
+                        cliente.setSegundoApellidoCliente("---");
+                        cliente.setTelefonoCliente("--------");
+                        cliente.setDireccionCliente("---");
+                        cliente.setSexoCliente(Cliente.SexoCliente.Hombre);
+                        cliente.setNitCliente("---");
+                        cliente.setEstado(Cliente.EstadoCliente.Activo);
+                        cliente.setCodigoUsuario(usuarioDao.obtenerCodigo(correo).getCodigoUsuario());
+                        
+                        clienteDao.agregar(cliente);
 
                         request.getRequestDispatcher("index.jsp").forward(request, response);
                     }
-                    break;
+                break;
+                
+                case "AgregarCrud":
+                    String correoCrud = request.getParameter("txtCorreoUsuario");
+                    String contrasenaCrud = request.getParameter("txtPasswordUsuario");
+                    Part archivo = request.getPart("txtFoto");
+                    InputStream is = archivo.getInputStream();
+                    
+                    usuario.setCorreoUsuario(correoCrud);
+                    usuario.setContrasenaUsuario(contrasenaCrud);
+                    usuario.setFoto(is);
+                    
+                    usuarioDao.agregar(usuario);
+                    request.getRequestDispatcher("Controlador?menu=Usuario&accion=Listar").forward(request, response);
+                break;
+                    
+                case "Editar":
+                    codUsuario = Integer.parseInt(request.getParameter("codigoUsuario"));
+                    Usuario us = usuarioDao.listarCodigoUsuario(codUsuario);
+                    request.setAttribute("usuarioEdit", us);
+                    request.getRequestDispatcher("Controlador?menu=Usuario&accion=Listar").forward(request, response);
+                break;
+                
+                case "Actualizar":
+                    String correoUsu = request.getParameter("txtCorreoUsuario");
+                    String contrasenaUsu = request.getParameter("txtPasswordUsuario");
+                    String fechaRegistro = request.getParameter("txtFechaRegistro");
+                    
+                    Part archivoAct = request.getPart("txtFoto");
+                    InputStream ist = archivoAct.getInputStream();
+                    
+                    Usuario.Cargo cargo = Usuario.Cargo.valueOf(request.getParameter("txtCargo"));
+                    
+                    usuario.setCorreoUsuario(correoUsu);
+                    usuario.setContrasenaUsuario(contrasenaUsu);
+                    usuario.setFechaRegistro(Date.valueOf(fechaRegistro));
+                    usuario.setFoto(ist);
+                    usuario.setCargo(cargo);
+                    usuario.setCodigoUsuario(codUsuario);
+                    
+                    usuarioDao.actualizar(usuario);
+                    request.getRequestDispatcher("Controlador?menu=Usuario&accion=Listar").forward(request, response);
+                break;
+                
+                case "Eliminar":
+                    codUsuario = Integer.parseInt(request.getParameter("codigoUsuario"));
+                    usuarioDao.eliminar(codUsuario);
+                    request.getRequestDispatcher("Controlador?menu=Usuario&accion=Listar").forward(request, response);
+                break;
             }
-
+            
+            if (accion.equals("Mover")) {
+                // ANIMACIÓN DE TRANSICIÓN NO TOCAR
+                request.setAttribute("jspFinal", "Controlador?menu=Usuario&accion=Listar");
+                request.getRequestDispatcher("Transicion.jsp").forward(request, response);
+            } else if (accion.equals("Listar")) {
+                request.getRequestDispatcher("Usuario.jsp").forward(request, response);
+            }
         } else if (menu.equals("Resena")) {
 
             switch (accion) {
